981215742536b7ffd5f42b1195e85311e27d5092
Darak222 <tobi_sobota@onet.eu>
2025-05-02 15:42:28 +0200

    Initial commit
    Initializing first files into the repository with basic functions and .gitignore file

---
+++ scripts/classes/main.py
@@ -0,0 +1,17 @@
+import discord
+from auth_key import token  # Ensure you have a token.py file with your bot token
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(token)
